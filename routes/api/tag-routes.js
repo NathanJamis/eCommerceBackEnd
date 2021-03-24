98e809530592db97498c62e5a65eee49cd3f8a50
Nate J <jamis.walsh@gmail.com>
2021-03-23 21:40:03 -0700

    added posts

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -8,7 +8,7 @@ router.get('/', async (req, res) => {
   // be sure to include its associated Product data
   try {
     const tagData = await Tag.findAll({
-      include: [{ model: Product }]
+      include: [{ model: Product, through: ProductTag }]
     });
     res.status(200).json(tagData);
   } catch (err) {
@@ -21,7 +21,7 @@ router.get('/:id', async (req, res) => {
   // be sure to include its associated Product data
   try {
     const tagData = await Tag.findByPk(req.params.id, {
-      include: [{ model: Product }]
+      include: [{ model: Product, through: ProductTag }]
     });
     if (!tagData) {
       res.status(404).json({ message: 'No tag found with this id.' });
@@ -35,6 +35,12 @@ router.get('/:id', async (req, res) => {
 
 router.post('/', async (req, res) => {
   // create a new tag
+  try {
+    const tagData = await Tag.create(req.body);
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', async (req, res) => {
